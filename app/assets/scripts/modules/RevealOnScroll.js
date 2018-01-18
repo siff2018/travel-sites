35f590f949127e97f63576ddaa9ac4fa3ea0fa37
Sif <sifddin@hotmail.com>
2018-01-18 12:27:26 +0100

    Completed sticky header functionality.

--- app/assets/scripts/modules/RevealOnScroll.js
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -4,7 +4,7 @@ import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoi
 class RevealOnScroll{
  constructor(els, offset ){
      this.itemsToReveal = els;
-     this.offsetPercentage = offset;
+      this.offsetPercentage = offset;
      this.hideInitially();
      this.createWaypoints();
  }
